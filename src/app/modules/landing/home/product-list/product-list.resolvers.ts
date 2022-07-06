2be09ac7896bc9ef0161e135bfed91869681711a
ticuong436 <55774540+ticuong436@users.noreply.github.com>
2022-07-06 21:24:05 +0700

    sanpham

---
+++ src/app/modules/landing/home/product-list/product-list.resolvers.ts
@@ -0,0 +1,109 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
+import { catchError, Observable, throwError } from 'rxjs';
+import { ProductListService } from './product-list.service';
+
+
+
+@Injectable({
+    providedIn: 'root'
+})
+
+
+export class DanhmucDetailResolver implements Resolve<any>
+{
+    /**
+     * Constructor
+     */
+    constructor(
+        private _productService: ProductListService,
+        private _router: Router
+    )
+    {
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * Resolver
+     *
+     * @param route
+     * @param state
+     */
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any>
+    {
+        const id = route.paramMap.get('id');
+        return this._productService.getDanhmucDetail(id)
+                   .pipe(
+                       // Error here means the requested contact is not available
+                       catchError((error) => {
+
+                           // Log the error
+                           console.error(error);
+
+                           // Get the parent url
+                           const parentUrl = state.url.split('/').slice(0, -1).join('/');
+                            
+                           // Navigate to there
+                           this._router.navigateByUrl(parentUrl);
+
+                           // Throw an error
+                           return throwError(error);
+                       })
+                   );
+    }
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+
+
+export class ProductDetailResolver implements Resolve<any>
+{
+    /**
+     * Constructor
+     */
+    constructor(
+        private _productService: ProductListService,
+        private _router: Router
+    )
+    {
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * Resolver
+     *
+     * @param route
+     * @param state
+     */
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any>
+    {
+        const id = route.paramMap.get('id');
+        return this._productService.getProductDetail(id)
+                   .pipe(
+                       // Error here means the requested contact is not available
+                       catchError((error) => {
+
+                           // Log the error
+                           console.error(error);
+
+                           // Get the parent url
+                           const parentUrl = state.url.split('/').slice(0, -1).join('/');
+                            
+                           // Navigate to there
+                           this._router.navigateByUrl(parentUrl);
+
+                           // Throw an error
+                           return throwError(error);
+                       })
+                   );
+    }
+}
+
--- src/app/modules/landing/home/product-list/sanpham-danhmuc/sanpham-danhmuc.component.ts
+++ src/app/modules/landing/home/product-list/sanpham-danhmuc/sanpham-danhmuc.component.ts
@@ -1,228 +1,233 @@
-import { LabelType, Options } from "@angular-slider/ngx-slider";
-import { FlatTreeControl } from "@angular/cdk/tree";
-import { Component, OnInit, ViewEncapsulation } from "@angular/core";
-import { MatTreeFlatDataSource, MatTreeFlattener } from "@angular/material/tree";
-import { Subject, take, takeUntil } from "rxjs";
-import { ThuonghieuService } from "../../thuonghieu/thuonghieu.service";
-import { ProductListService } from "../product-list.service";
+import { LabelType, Options } from '@angular-slider/ngx-slider';
+import { FlatTreeControl } from '@angular/cdk/tree';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+    MatTreeFlatDataSource,
+    MatTreeFlattener,
+} from '@angular/material/tree';
+import { ActivatedRoute } from '@angular/router';
+import { Subject, take, takeUntil } from 'rxjs';
+import { ThuonghieuService } from '../../thuonghieu/thuonghieu.service';
+import { ProductListService } from '../product-list.service';
 
 interface ExampleFlatNode {
-  expandable: boolean;
-  name: string;
-  level: number;
+    expandable: boolean;
+    name: string;
+    level: number;
 }
 
 @Component({
-  selector: "app-sanpham-danhmuc",
-  templateUrl: "./sanpham-danhmuc.component.html",
-  styleUrls: ["./sanpham-danhmuc.component.scss"],
-  encapsulation: ViewEncapsulation.None
+    selector: 'app-sanpham-danhmuc',
+    templateUrl: './sanpham-danhmuc.component.html',
+    styleUrls: ['./sanpham-danhmuc.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
-
 export class SanphamDanhmucComponent implements OnInit {
-  selectedSendingFeatures;
-  products: any[];
-  iCheckbox = [];
-  tempProductSplice: any[] = [];
-  productDM: any[];
-  danhmuc: any[];
-  valuePrice: number;
-  selectedIndex: number;
-  productListhide: number;
-  minValue: number = 0;
-  temp: any[];
-  maxValue: number = 5000000;
-  isChecked = false;
-  indexPaginate: number = 0;
-  arrCheckboxThuonghieu = [];
-  filterThuonghieu = [];
-  tempAllProducts: any[] = [];
-  tempDM: any[] = [];
-  private _unsubscribeAll: Subject<any> = new Subject<any>();
+    selectedSendingFeatures;
+    products: any[];
+    iCheckbox = [];
+    tempProductSplice: any[] = [];
+    productDM: any[];
+    danhmuc: any[];
+    valuePrice: number;
+    selectedIndex: number;
+    productListhide: number;
+    minValue: number = 0;
+    temp: any[];
+    maxValue: number = 100;
+    isChecked = false;
+    indexPaginate: number = 0;
+    arrCheckboxThuonghieu = [];
+    filterThuonghieu = [];
+    tempAllProducts: any[] = [];
+    tempDM: any[] = [];
+    private _unsubscribeAll: Subject<any> = new Subject<any>();
 
-  private _transformer = (node: any, level: number) => {
-    return {
-        expandable: !!node.children && node.children.length > 0,
-        name: node.Tieude || node.title,
-        level: level,
-        item: node,
+    private _transformer = (node: any, level: number) => {
+        return {
+            expandable: !!node.children && node.children.length > 0,
+            name: node.Tieude || node.title,
+            level: level,
+            item: node,
+        };
     };
-};
 
-treeControl = new FlatTreeControl<ExampleFlatNode>(
-    (node) => node.level,
-    (node) => node.expandable
-);
-
-treeFlattener = new MatTreeFlattener(
-    this._transformer,
-    (node) => node.level,
-    (node) => node.expandable,
-    (node) => node.children
-);
-
-dataSource = new MatTreeFlatDataSource(
-    this.treeControl,
-    this.treeFlattener
-);
-
-  options: Options = {
-    floor: 0,
-    ceil: 500000,
-    translate: (value: number, label: LabelType): string => {
-      switch (label) {
-        case LabelType.Low:
-          return "<b>Min price:</b>" + value + "đ";
-        case LabelType.High:
-          return "<b>Max price:</b>" + value + "đ";
-        default:
-          this.valuePrice = value;
-
-          return  value + "đ";
-      }
-    },
-  };
-  thuonghieus: string[] = [];
-  changePrice(value) {
-    this._productService.products$.pipe(take(1)).subscribe((res) => {
-      
-      let arr = res.filter((x) => x.Gia <= value);
-      if (arr.length > 0) {
-        this.splceArr(arr);
-      }
-    });
-  }
-  productListtoggle(number) {
-    this.productListhide = number;
-  }
-  constructor(
-    private _productService: ProductListService,
-    private _thuonghieuService: ThuonghieuService
-  ) {
-    this.productListhide = 1;
-  }
-  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
-
-  ngOnDestroy(): void {
-    this._unsubscribeAll.next(null);
-    this._unsubscribeAll.complete();
-  }
-  onSelectDanhmuc(item, i) {
-    console.log(item);
-
-    this.tempProductSplice = [];
-    this.selectedIndex = i;
-    let a = [];
-    this.tempDM.filter((x) => {
-      if (x.pid == item.id) {
-        a.push(x);
-      }
-    });
-    if (a.length > 1) {
-      a = [...a, item];
-    } else {
-      a = [item];
-    }
-
-    let b = [];
-
-    this._productService.products$.subscribe((res) => {
-      res.filter((x) => {
-        a.forEach((v) => {
-          if (x.idDM == v.id) {
-            b.push(x);
-          }
-        });
-      });
-    });
-    this.splceArr(b);
-  }
-
-  paginateNumber(i) {
-    this.indexPaginate = i;
-    console.log(i);
-
-    this.productDM = this.tempProductSplice[i];
-  }
-  selectSale() {
-    this.isChecked = !this.isChecked;
-
-    if (this.isChecked == true) {
-      this.productDM = this.productDM.filter((x) => x.Trangthai == "1");
-    } else {
-      this.productDM = this.temp;
-    }
-  }
-  checkboxThuonghieu(item, i) {
-    this._productService.products$.pipe(take(1)).subscribe((res) => {
-      
-      let arr = res.filter((x) => x.Thuonghieu == item.id);
-      if (arr.length > 0) {
-        this.splceArr(arr);
-      }
-    });
-  }
-
-  sapxepgiatri(value){
-    if (value == "low") {
-      let arr
-      this._productService.products$.subscribe((res) =>
-      arr =   res.sort((a, b) => {
-          return a.Gia - b.Gia;
-        })
-      );
-      console.log(arr);
-      
-      this.splceArr(arr)
-    }
-  }
-
-  splceArr(arr) {
-    this.tempProductSplice = [];
-    let x = arr.length / 12;
-    if (arr.length > 0) {
-      for (let i = 0; i < x; i++) {
-        this.tempProductSplice.push(arr.splice(0, 12));
-        console.log(this.tempProductSplice);
-      }
-    }
-    this.productDM = this.tempProductSplice[0];
-  }
-  nest = (items, id = "", link = "pid") =>
-    items
-      ?.filter((item) => item[link] == id)
-      .map((item) => ({
-        ...item,
-        children: this.nest(items, item.id),
-      }));
-
-  ngOnInit(): void {
-    this._productService.getDanhmuc().pipe(take(1)).subscribe();
-    this._productService.danhmuc$.subscribe((res) => {
-      this.tempDM = res;
-      this.danhmuc = this.nest(res);
-      this.dataSource.data = this.danhmuc 
-    });
-    this._productService.getProduct().subscribe();
-    this._productService.products$.pipe(take(1)).subscribe((res) => {
-      if (res) {
-        this.products = res;
-
-        let arr = res;
-        let x = arr?.length / 12;
-
-        if (arr.length > 0) {
-          for (let i = 0; i < x; i++) {
-            this.tempProductSplice.push(arr.splice(0, 12));
-          }
-        }
-      }
-      this.productDM = this.tempProductSplice[0];
-      this.temp = this.productDM;
-    });
-    this._thuonghieuService.getThuonghieu().subscribe();
-    this._thuonghieuService.thuonghieus$.subscribe(
-      (res) => (this.thuonghieus = res)
+    treeControl = new FlatTreeControl<ExampleFlatNode>(
+        (node) => node.level,
+        (node) => node.expandable
     );
-  }
+
+    treeFlattener = new MatTreeFlattener(
+        this._transformer,
+        (node) => node.level,
+        (node) => node.expandable,
+        (node) => node.children
+    );
+
+    dataSource = new MatTreeFlatDataSource(
+        this.treeControl,
+        this.treeFlattener
+    );
+
+    options: Options = {
+        floor: 0,
+        ceil: 100,
+        translate: (value: number, label: LabelType): string => {
+            switch (label) {
+                case LabelType.Low:
+                    return '<b>Min price:</b>' + value + 'đ';
+                case LabelType.High:
+                    return '<b>Max price:</b>' + value + 'đ';
+                default:
+                    this.valuePrice = value;
+
+                    return value + 'đ';
+            }
+        },
+    };
+    thuonghieus: string[] = [];
+    changePrice(value) {
+        this._productService.products$.pipe(take(1)).subscribe((res) => {
+            let arr = res.filter((x) => x.Gia <= value);
+            if (arr.length > 0) {
+                this.splceArr(arr);
+            }
+        });
+    }
+    productListtoggle(number) {
+        this.productListhide = number;
+    }
+    constructor(
+        private _productService: ProductListService,
+        private _thuonghieuService: ThuonghieuService,
+        private _route: ActivatedRoute
+    ) {
+        this.productListhide = 1;
+    }
+    hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
+
+    ngOnDestroy(): void {
+        this._unsubscribeAll.next(null);
+        this._unsubscribeAll.complete();
+    }
+    // onSelectDanhmuc(item, i) {
+    //     this.tempProductSplice = [];
+    //     this.selectedIndex = i;
+    //     let a = [];
+    //     this.tempDM.filter((x) => {
+    //         if (x.pid == item.id) {
+    //             a.push(x);
+    //         }
+    //     });
+    //     if (a.length > 1) {
+    //         a = [...a, item];
+    //     } else {
+    //         a = [item];
+    //     }
+
+    //     let b = [];
+
+    //     this._productService.products$.subscribe((res) => {
+    //         res.filter((x) => {
+    //             a.forEach((v) => {
+    //                 if (x.idDM == v.id) {
+    //                     b.push(x);
+    //                 }
+    //             });
+    //         });
+    //     });
+    //     this.splceArr(b);
+    // }
+
+    paginateNumber(i) {
+        this.indexPaginate = i;
+        console.log(i);
+
+        this.productDM = this.tempProductSplice[i];
+    }
+    selectSale() {
+        this.isChecked = !this.isChecked;
+
+        if (this.isChecked == true) {
+            this.productDM = this.productDM.filter((x) => x.Trangthai == '1');
+        } else {
+            this.productDM = this.temp;
+        }
+    }
+    checkboxThuonghieu(item, i) {
+        this._productService.products$.pipe(take(1)).subscribe((res) => {
+            let arr = res.filter((x) => x.Thuonghieu == item.id);
+            if (arr.length > 0) {
+                this.splceArr(arr);
+            }
+        });
+    }
+
+    sapxepgiatri(value) {
+        if (value == 'low') {
+            let arr;
+            this._productService.products$.subscribe(
+                (res) =>
+                    (arr = res.sort((a, b) => {
+                        return a.Gia - b.Gia;
+                    }))
+            );
+            console.log(arr);
+
+            this.splceArr(arr);
+        }
+    }
+
+    splceArr(arr) {
+        this.tempProductSplice = [];
+        let x = arr.length / 12;
+        if (arr.length > 0) {
+            for (let i = 0; i < x; i++) {
+                this.tempProductSplice.push(arr.splice(0, 12));
+                console.log(this.tempProductSplice);
+            }
+        }
+        this.productDM = this.tempProductSplice[0];
+    }
+    nest = (items, id = '', link = 'pid') =>
+        items
+            ?.filter((item) => item[link] == id)
+            .map((item) => ({
+                ...item,
+                children: this.nest(items, item.id),
+            }));
+
+    ngOnInit(): void {
+        this._route.params.subscribe((data) => {
+            this._productService.getDanhmuc().pipe(take(1)).subscribe();
+            this._productService.danhmuc$.subscribe((res) => {
+                this.tempDM = res;
+                this.danhmuc = this.nest(res);
+                this.dataSource.data = this.danhmuc;
+            });
+            this._productService.getProduct().subscribe();
+            this._productService.products$.pipe(take(1)).subscribe((res) => {
+                if (res) {
+                    this.products = res.filter((x) => x.idDM == data.id);
+                    console.log(this.products);
+
+                    let x = this.products?.length / 12;
+
+                    if (this.products?.length > 0) {
+                        for (let i = 0; i < x; i++) {
+                            this.tempProductSplice.push(
+                                this.products.splice(0, 12)
+                            );
+                        }
+                    }
+                }
+                this.productDM = this.tempProductSplice[0];
+                this.temp = this.productDM;
+            });
+            this._thuonghieuService.getThuonghieu().subscribe();
+            this._thuonghieuService.thuonghieus$.subscribe(
+                (res) => (this.thuonghieus = res)
+            );
+        });
+    }
 }
